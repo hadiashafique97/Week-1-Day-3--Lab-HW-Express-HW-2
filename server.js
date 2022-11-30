0031cc0b54c75b90ca83cabf4e27ae253dae756e
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2022-11-30 10:32:15 -0600

    tip calulcator

--- server.js
+++ server.js
@@ -10,11 +10,22 @@ const port = 3000
 
 
 
-
+//Greeting
 app.get('/greeting', (req, res)=>{
     res.send('Hello Stranger')
 })
 
+//Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    const tip = req.params.total * req.params.tipPercentage
+   const actualTip = tip/100
+    
+    res.send('Congrats you earned a tip of $'+ actualTip)
+})
+
+
+
+//Greeting :name
 app.get('/greeting/:name', (req, res)=>{
     res.send('Hello ,' +req.params.name )
 })
