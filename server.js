e7e0c760b8450714adbd0bf5806dfdefd005ac41
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2022-11-30 12:06:44 -0600

    magic 8 ball

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ app.get('/greeting', (req, res)=>{
     res.send('Hello Stranger')
 })
 
+
+
+
+//MAGIC 8 BALL 
+const eightBall= ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:WillIbeaMillionaire', (req, res)=>{
+    var randomItem = eightBall[Math.floor(Math.random()*eightBall.length)]
+    res.send(req.params.WillIbeaMillionaire + ' '+randomItem)
+})
+
+
+
 //Tip Calculator
 app.get('/tip/:total/:tipPercentage', (req, res)=>{
     const tip = req.params.total * req.params.tipPercentage
